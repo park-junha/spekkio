5415db08d2a0bebb62a3e4b07c962576d839fe0b
Junha Park <jpark3@scu.edu>
2021-05-19 16:40:51 -0700

    (1.1.0) Support no-ff merges

--- src/git_merge/main.sh
+++ src/git_merge/main.sh
@@ -16,11 +16,15 @@ function rollback_gitconfig {
 }
 
 function rollback_commit {
-  git reset --hard HEAD~1
+  if [[ $# -gt 0 ]]; then
+    git reset --hard $1
+  else
+    git reset --hard HEAD~1
+  fi
 }
 
 function rollback_commit_and_tag {
-  git tag --delete $tag
+  git tag --delete $1
   rollback_commit
 }
 
@@ -116,7 +120,7 @@ if [[ $# -eq 6 ]]; then
       echo "err: \"git push --atomic origin \"$base_branch\" \"$tag\"\""
       echo "     returned $rc"
       echo "rolling back commit and tag..."
-      rollback_commit_and_tag
+      rollback_commit_and_tag $tag
       echo "rolling back temporary git config"
       rollback_gitconfig
       exit $rc
@@ -132,8 +136,103 @@ if [[ $# -eq 6 ]]; then
     exit 1
     ;;
   merge)
-    echo "err: sorry, param merge is not yet supported"
-    exit 1
+    # Checkout to base branch and pull
+    git checkout $base_branch && git pull
+    rc=$?
+    if [[ $rc -ne 0 ]]; then
+      echo "err: \"git checkout $base_branch && git pull\" returned $rc"
+      exit $rc
+    fi
+
+    # Preserve last commit of base branch
+    last_commit=$(git rev-parse HEAD)
+
+    # Get author info from branch
+    git checkout $branch
+    rc=$?
+    if [[ $rc -ne 0 ]]; then
+      echo "err: \"git checkout $branch\" returned $rc"
+      exit $rc
+    fi
+    # TODO: below line assumes there is only one author
+    author=$(git log $base_branch.. | grep Author | sed 's/Author: //' | \
+      uniq)
+
+    # Get pre existing local configs if they exist
+    username_before=$(git config --local --list | egrep 'user\.name' | \
+      sed 's/user\.name=//')
+    useremail_before=$(git config --local --list | egrep 'user\.email' | \
+      sed 's/user\.email=//')
+
+    # Set (bot) committer info
+    git config --local user.name "$BOT_USERNAME"
+    echo "applying temporary git config user\.name: $BOT_USERNAME"
+    git config --local user.email "$BOT_EMAIL"
+    echo "applying temporary git config user\.email: $BOT_EMAIL"
+
+    # Checkout to base branch and pull again
+    git checkout $base_branch && git pull
+    rc=$?
+    if [[ $rc -ne 0 ]]; then
+      echo "err: \"git checkout $base_branch && git pull\" returned $rc"
+      echo "rolling back temporary git config"
+      rollback_gitconfig
+      exit $rc
+    fi
+
+    # Merge without fast forward
+    git merge --no-ff $branch
+    rc=$?
+    if [[ $rc -ne 0 ]]; then
+      echo "err: \"git merge --no-ff $base_branch\" returned $rc"
+      echo "rolling back temporary git config"
+      rollback_gitconfig
+      exit $rc
+    fi
+
+    # Commit
+    git commit --amend --message="$commit_message" --author="$author"
+    rc=$?
+    if [[ $rc -ne 0 ]]; then
+      echo "err: \"git commit --amend\" returned $rc"
+      echo "unstaging and discaring all items in working tree..."
+      rollback_commit $last_commit
+      echo "rolling back temporary git config"
+      rollback_gitconfig
+      exit $rc
+    fi
+
+    # Tag
+    git tag --annotate $tag --message="$tag_message"
+    rc=$?
+    if [[ $rc -ne 0 ]]; then
+      echo "err: \"git tag --annotate $tag --message=\"$tag_message\"\""
+      echo "     returned $rc"
+      echo "rolling back commit..."
+      rollback_commit $last_commit
+      echo "rolling back temporary git config"
+      rollback_gitconfig
+      exit $rc
+    fi
+
+    # Push commit and tag
+    git push --atomic origin "$base_branch" "$tag"
+    rc=$?
+    if [[ $rc -ne 0 ]]; then
+      echo "err: \"git push --atomic origin \"$base_branch\" \"$tag\"\""
+      echo "     returned $rc"
+      echo "rolling back commit and tag..."
+      git tag --delete $tag
+      rollback_commit $last_commit
+      echo "rolling back temporary git config"
+      rollback_gitconfig
+      exit $rc
+    fi
+
+    # Unset (bot) committer info OR reset to configs before if they existed
+    rollback_gitconfig
+
+    exit 0
     ;;
   *)
     echo "err: invalid param $1"
